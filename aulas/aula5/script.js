00ab158d499964880f4fae30f81bb12db50d6e2a
JoviMartinelle <joao.martinelle12@gmail.com>
2026-03-26 20:40:39 -0300

    0.3.4 - Botões levam pro github e linkedin

--- aulas/aula5/script.js
+++ aulas/aula5/script.js
@@ -1,3 +1,17 @@
-// Exemplo simples (pra você expandir depois)
+const botaoCodigo = document.querySelector('#btnCodigo');
 
-console.log("Site carregado com sucesso!");
+if (botaoCodigo) {
+    botaoCodigo.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.open('https://github.com/luizamantovani/Projeto-Desenvolvimento-Web', '_blank');
+    });
+}
+
+const botaoCodigo2 = document.querySelector('#btnCodigo2');
+
+if (botaoCodigo2) {
+    botaoCodigo2.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.open('https://www.linkedin.com/in/joão-vitor-martinelle-160b73272?utm_source=share_via&utm_content=profile&utm_medium=member_ios', '_blank');
+    });
+}
