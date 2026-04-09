0c4361582dc567c77d6bedda29987e1d84968e4d
Luiza Mantovani <luizamantovani2006@gmail.com>
2026-04-09 19:34:18 -0300

    0.5.0 - Adicionei a funcionalidade to do list

---
+++ aulas/aula6/src/pages/ToDoList/ToDoList.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './ToDoList.css';
+
+const ToDoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
+      setInput('');
+    }
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div className="todo-list">
+      <h1 className="todo-title">ToDo List</h1>
+
+      <div className="input-container">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+
+      <ul>
+        {todos.map(todo => (
+          <li
+            key={todo.id}
+            className={`todo-item ${todo.completed ? 'completed' : ''}`}
+          >
+            <span
+              className="task-text"
+              onClick={() => toggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+
+            <button
+              className="delete-btn"
+              onClick={() => deleteTodo(todo.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
