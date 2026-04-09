675893aa8ac602d5ef27187827b22cfa8e0f0c06
Luiza Mantovani <luizamantovani2006@gmail.com>
2026-04-09 19:27:07 -0300

    0.1.0 - Adicionei o header

--- aulas/aula6/src/App.js
+++ aulas/aula6/src/App.js
@@ -14,34 +14,9 @@ function App() {
   });
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Funcionalidades</h1>
-        <nav className="App-nav">
-          <a href="#todo" onClick={() => setMostrarResultado({ ...mostrarResultado, ToDoList: true, ContadorDeCliques: false, JogoDaVelha: false, FormularioCEP: false })}>To Do List</a>
-          <a href="#contador" onClick={() => setMostrarResultado({ ...mostrarResultado, ContadorDeCliques: true, ToDoList: false, JogoDaVelha: false, FormularioCEP: false })}>Contador de Cliques</a>
-          <a href="#jogo" onClick={() => setMostrarResultado({ ...mostrarResultado, JogoDaVelha: true, ToDoList: false, ContadorDeCliques: false, FormularioCEP: false })}>Jogo da Velha</a>
-          <a href="#cep" onClick={() => setMostrarResultado({ ...mostrarResultado, FormularioCEP: true, ToDoList: false, ContadorDeCliques: false, JogoDaVelha: false })}>Buscador de CEPs</a>
-        </nav>
-      </header>
+      <header></header>
 
-      <main className="App-main">
-        {mostrarResultado.ToDoList && (
-          <ToDoList />
-
-        )}
-
-        {mostrarResultado.ContadorDeCliques && (
-          <ContadorDeCliques />
-        )}
-
-        {mostrarResultado.JogoDaVelha && (
-          <JogoDaVelha />
-        )}
-
-        {mostrarResultado.FormularioCEP && (
-          <FormularioCEP />
-        )}
-      </main>
+      
     </div>
   );
 }
