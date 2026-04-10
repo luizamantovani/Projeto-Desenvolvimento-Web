d63235fea2893de3ad01f252073444c6952a67b9
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-04-09 23:39:19 -0300

    Adiciona estrutura inicial da parte Backend do projeto com Maven, incluindo arquivos de configuração e classes principais

---
+++ project/agendo/backend/src/main/java/com/devweb/agendo/AgendoApplication.java
@@ -0,0 +1,13 @@
+package com.devweb.agendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendoApplication.class, args);
+	}
+
+}
