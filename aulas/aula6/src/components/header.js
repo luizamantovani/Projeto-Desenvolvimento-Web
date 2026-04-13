8902dea4ad217cc9e276760896b72c4aabd30431
Luiza Mantovani <luizamantovani2006@gmail.com>
2026-04-13 20:58:17 -0300

    0.7.2 - Corrige bug que fazia aparecer a calculadora quando clicava em outras telas

--- aulas/aula6/src/components/header.js
+++ aulas/aula6/src/components/header.js
@@ -1,4 +1,5 @@
 import '../App.css';
+import Calculadora from '../pages/Calculadora/Calculadora';
 
 
 function Header({ mostrarResultado, setMostrarResultado }) {
@@ -6,10 +7,10 @@ function Header({ mostrarResultado, setMostrarResultado }) {
     <header className="App-header">
       <h1>Funcionalidades</h1>
       <nav className="App-nav">
-        <a href="#todo" onClick={() => setMostrarResultado({ ...mostrarResultado, ToDoList: true, ContadorDeCliques: false, JogoDaVelha: false, FormularioCEP: false })}>To Do List</a>
-        <a href="#contador" onClick={() => setMostrarResultado({ ...mostrarResultado, ContadorDeCliques: true, ToDoList: false, JogoDaVelha: false, FormularioCEP: false })}>Contador de Cliques</a>
-        <a href="#jogo" onClick={() => setMostrarResultado({ ...mostrarResultado, JogoDaVelha: true, ToDoList: false, ContadorDeCliques: false, FormularioCEP: false })}>Jogo da Velha</a>
-        <a href="#cep" onClick={() => setMostrarResultado({ ...mostrarResultado, FormularioCEP: true, ToDoList: false, ContadorDeCliques: false, JogoDaVelha: false })}>Buscador de CEPs</a>
+        <a href="#todo" onClick={() => setMostrarResultado({ ...mostrarResultado, ToDoList: true, ContadorDeCliques: false, JogoDaVelha: false, FormularioCEP: false, Calculadora: false })}>To Do List</a>
+        <a href="#contador" onClick={() => setMostrarResultado({ ...mostrarResultado, ContadorDeCliques: true, ToDoList: false, JogoDaVelha: false, FormularioCEP: false, Calculadora: false })}>Contador de Cliques</a>
+        <a href="#jogo" onClick={() => setMostrarResultado({ ...mostrarResultado, JogoDaVelha: true, ToDoList: false, ContadorDeCliques: false, FormularioCEP: false, Calculadora: false })}>Jogo da Velha</a>
+        <a href="#cep" onClick={() => setMostrarResultado({ ...mostrarResultado, FormularioCEP: true, ToDoList: false, ContadorDeCliques: false, JogoDaVelha: false, Calculadora: false })}>Buscador de CEPs</a>
         <a href="#calculadora" onClick={() => setMostrarResultado({ ...mostrarResultado, Calculadora: true, ToDoList: false, ContadorDeCliques: false, JogoDaVelha: false, FormularioCEP: false })}>Calculadora</a>
       </nav>
     </header>
