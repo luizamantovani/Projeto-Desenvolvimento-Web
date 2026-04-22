b691cbe4eebb36446cecaedbd538e95b77a2b478
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-04-22 20:16:46 -0300

    feat: adiciona entidade Sessao com repositório e enum de status

--- project/agendo/backend/src/main/java/com/devweb/agendo/repository/SessaoRepository.java
+++ project/agendo/backend/src/main/java/com/devweb/agendo/repository/SessaoRepository.java
@@ -1,10 +1,10 @@
 package com.devweb.agendo.repository;
 
-import com.devweb.agendo.model.Materia;
+import com.devweb.agendo.model.Sessao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MateriaRepository extends JpaRepository<Materia, Long> {
+public interface SessaoRepository extends JpaRepository<Sessao, Long> {
 
 }
