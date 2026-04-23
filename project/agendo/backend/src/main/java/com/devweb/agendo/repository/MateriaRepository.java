be888bd3e1be3af1812e274bff4ca19fe8a59aaa
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-04-23 15:21:53 -0300

    feat: adiciona métodos de consulta e deleção no repositório de Sessao e Materia

--- project/agendo/backend/src/main/java/com/devweb/agendo/repository/MateriaRepository.java
+++ project/agendo/backend/src/main/java/com/devweb/agendo/repository/MateriaRepository.java
@@ -4,7 +4,9 @@ import com.devweb.agendo.model.Materia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MateriaRepository extends JpaRepository<Materia, Long> {
-
+    List<Materia> findByUsuarioId(Long usuarioId);
 }
